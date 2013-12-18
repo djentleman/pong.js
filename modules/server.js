cb3fc1d30c8d0d4b285b3509b27cbf666fdd9d1c
Todd Perry <up612136@myport.ac.uk>
2013-12-18 00:24:58 +0000

    reorganized socket io code

--- modules/server.js
+++ modules/server.js
@@ -25,130 +25,12 @@ function start(){
     //console.log("games json initialized");
     function onRequest(request, response) {
         if (url.parse(request.url).pathname != "/favicon.ico"){ 
-            if (request.method != "POST"){
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlStr);
+            response.end();
 
-
-
-                
-
-
-
-
-
-
-
-
-                //console.log(games);
-                // no need to route favicon requests in this hot rockstar app
-                var myGame;
-                var myPlayer;
-                var gameFound = false;
-                for(var i=0; i<games.length; i++ ){
-                    if (games[i].p1.ip == request.connection.remoteAddress){
-                        console.log("user already in game as player " + 1);
-                        myGame = i;
-                        gameFound = true;
-                        myPlayer = 1;
-                        break
-                    }
-                    if (games[i].p2 != undefined && games[i].p2.ip == request.connection.remoteAddress){
-                        console.log("user already in game as player " + 2);
-                        myGame = i;
-                        gameFound = true;
-                        myPlayer = 2;
-                        break
-                    }
-                    if (games[i].p2 == undefined){
-                        games[i].p2 = {"ip": request.connection.remoteAddress,
-                                       "score": 0,
-                                       "name": "",
-                                       "paddle": 0
-                                      }      
-                        console.log("user added to game" + games[i]);
-                        // start a new game
-                        myGame = i;
-                        myPlayer = 2;
-                        gameFound = true;
-                        break;
-                    }
-                }
-                if (gameFound == false){
-                    games.push({"p1" : {"ip": request.connection.remoteAddress,
-                                        "score": 0,
-                                        "name": "",
-                                        "paddle": 0}, 
-                                "ball": {"x": 0, "y": 0, "i": 0, "j": 0}});
-    
-                    console.log("newGameCreated");
-                    myGame = games.length - 1
-                    myPlayer = 1;
-                }
-
-
-                console.log(games);
-
-
-
-                socket.sockets.on('connection', function (socket) { // a sort of state machine needs to exist here
-                    socket.emit('init', {"id": myGame, "game": games[myGame], "player": myPlayer}); // initialze the game
-                    var updated = false;
-                    setInterval(function(){
-                        socket.emit('ballPacket', games[myGame].ball);
-                        if (games[myGame].p2 != undefined && !updated){
-                            // contact player 1 of the game
-                            socket.emit('player2Join', {"id": myGame, "game": games[myGame]})
-                            console.log("emiiting player 2 join...")
-                            updated = true;
-                        }
-                    }, 50);
-                });
-                //console.log(request.connection.remoteAddress);
-                
-                /*if (request.method == "POST"){
-                    gpd.getPost(request, function(returnVal) {
-                        //console.log(returnVal);
-                        if (myPlayer == 1){
-                            games[myGame].p1Y = returnVal.yCoord;
-                        }
-                        if (myPlayer == 2){
-                            games[myGame].p2Y = returnVal.yCoord;
-                        }
-                    });
-                    if (myPlayer == 1){
-                        if (games[myGame].p2Y != undefined){
-                            response.write(games[myGame].p2Y);
-                        }
-                        else{
-                            response.write("");
-                        }
-                    }
-                    if (myPlayer == 2){
-                        if (games[myGame].p2Y != undefined){
-                            response.write(games[myGame].p1Y);
-                        }
-                        else{
-                            response.write("");
-                        }
-                    }
-                    response.end();
-                }
-                */
-                response.writeHead(200, {"Content-Type": "text/html"});
-                response.write(htmlStr);
-                response.end();
-
-                
-
-
-            }
-
-            //console.log("gamesJSONs: " + games);
             
         }
-
-        
-
-        
     }
 
     
@@ -159,12 +41,130 @@ function start(){
     })
 
     httpServer = http.createServer(onRequest, ipv4).listen(1234); // port it's listening on
-    socket = io.listen(httpServer, { log: false });
+    io = io.listen(httpServer, { log: false });
 
-    socket.set('origins', "*:*");
+    io.set('origins', "*:*");
 
     console.log("The Sevrer Has Started");
     console.log("Running on " + ipv4  + ":1234")
+
+    io.sockets.on('connection', function (socket) { // a sort of state machine needs to exist here
+                    
+
+        var myGame;
+        var myPlayer;
+        var myId;
+        var gameFound = false;
+        for(var i=0; i<games.length; i++ ){
+            if (games[i].p1.ip == socket.handshake.address.address){
+                console.log("user already in game as player " + 1);
+                myGame = i;
+                gameFound = true;
+                myPlayer = 1;
+                break
+            }
+            if (games[i].p2 != undefined && games[i].p2.ip == socket.handshake.address.address){
+                console.log("user already in game as player " + 2);
+                myGame = i;
+                gameFound = true;
+                myPlayer = 2;
+                break
+            }
+            if (games[i].p2 == undefined){
+                games[i].p2 = {"ip": socket.handshake.address.address,
+                               "score": 0,
+                               "name": "",
+                               "id": null,
+                               "paddle": 0
+                              }      
+                console.log("user added to game" + games[i]);
+                // start a new game
+                myGame = i;
+                myPlayer = 2;
+                gameFound = true;
+                break;
+            }
+        }
+        if (gameFound == false){
+            games.push({"p1" : {"ip": socket.handshake.address.address,
+                                "score": 0,
+                                "name": "",
+                                "id": null,
+                                "paddle": 0}, 
+                        "ball": {"x": 0, "y": 0, "i": 0, "j": 0}});
+    
+            console.log("newGameCreated");
+            myGame = games.length - 1
+            myPlayer = 1;
+        }
+
+
+        console.log(games);
+        console.log(myGame);
+
+
+
+        // should only be sent to ONE SOCKET
+        socket.emit('init', {"id": myGame, "game": games[myGame], "player": myPlayer}); // initialze the game
+        var updated = false;
+
+
+        socket.on('startGame', function (gameId){
+            games[gameId].ball.i = 0.01;
+            games[gameId].ball.j = 0.005;
+        })
+
+        socket.on('paddlePacket', function (data){
+            if (myPlayer == 1){
+                games[myGame].p1.paddle = data;
+            } else {
+                games[myGame].p2.paddle = data;
+            }
+        })
+        setInterval(function(){
+            socket.emit('ballPacket', games[myGame]);
+            if (games[myGame].p2 != undefined && !updated){
+                // contact player 1 of the game
+                socket.emit('player2Join', {"id": myGame, "game": games[myGame]})
+                console.log("emiiting player 2 join...")
+                updated = true;
+            }
+
+            // move ball
+            games[myGame].ball.x += games[myGame].ball.i;
+            games[myGame].ball.y += games[myGame].ball.j;
+
+            // handle collisions
+
+            // check for collisions
+            // the ball will potentially colide if it's X coord is
+            // +-5 - paddle width - ball width (+-5 - 0.25 - 0.2)
+            if (games[myGame].ball.x >= 4.55){
+                // potential collision with right paddle
+                // abs(ball.y - right.y) needs to be less than 1
+                if (Math.abs(games[myGame].ball.y - games[myGame].p2.paddle) <= 1){
+                    games[myGame].ball.i *= -1
+                }
+            } else if (games[myGame].ball.x <= -4.55){
+                // potential collision with left paddle
+                // abs(ball.y - left.y) needs to be less than 1
+                if (Math.abs(games[myGame].ball.y - games[myGame].p1.paddle) <= 1){
+                    games[myGame].ball.i *= -1
+                }
+            }
+            // lower than floor : ballY >= (5 - half ball height - half the cieling/floor height)
+            if (games[myGame].ball.y <= (5 - 0.2 - 1.75)){
+
+
+                games[myGame].ball.j *= -1
+            }if (games[myGame].ball.y >= -(5 - 0.2 - 1.75)){
+                games[myGame].ball.j *= -1
+            }
+
+        }, 10); // framerate
+
+        
+    });
 }
 
 exports.start = start;
